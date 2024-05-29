45e96dae50f51d7fa3b32721129e9964ebd3af95
Artak Sarkisyan <sarkisyan.artak92@gmail.com>
2024-05-29 17:58:12 +0300

    Create Template about.html

--- store/views.py
+++ store/views.py
@@ -6,3 +6,5 @@ from store.models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+def about(request):
+    return render(request, 'store/about.html', {})
